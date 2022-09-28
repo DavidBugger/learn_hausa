e8d2a6633f3a071817c26cd4ef55d3f6661161fe
David_Bugger <bwehfukdavid@gmail.com>
2022-09-28 11:51:45 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/bugger/learn_hausa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bugger.learn_hausa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/strings/introduction_string.dart
@@ -0,0 +1,19 @@
+const firstIntrTitleText = 'Welcome';
+const secondIntrTitleText = 'Introduction';
+const thirdIntrTitleTextt = 'Explore More';
+
+const firstIntrContentText =
+    'Welcome to Hausa Learning App happy learning!';
+
+const secondIntrContentText =
+    'is a mobile app that helps in teaching Hausa as a language for corp members as well \n individuals who want to learn the language  ';
+
+const thirdIntrContentText =
+    'Happy learning!!';
+
+// final firstImage = 'https://img.freepik.com/free-vector/smiling-chef-cartoon-character_8250-10.jpg';
+// final secondImage = 'https://pngquant.org/pngquant-logo-small.png';
+// final thirdImage = 'https://pngquant.org/pngquant-logo-small.png';
+// final firstImageIcon = 'https://pngquant.org/pngquant-logo-small.png';
+// final secondImageIcon = 'https://pngquant.org/pngquant-logo-small.png';
+// final thirdImageIcon = 'https://pngquant.org/pngquant-logo-small.png';
